e210bcc82d6ca88ccbc1237300364589da0adea8
Arash Nouri <arash.nouri@testudosrl.com>
2022-12-12 14:15:47 +0100

    package update

---
+++ src/__tests__/tags/tags.test.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Tag } from '../../components/tag/tags';
+
+describe('Tags component', () => {
+  it('should return default message when no tags and no message are passed', () => {
+    // default message = 'No tags to show'
+    const data: string[] = [];
+
+    expect(<Tag data={data} />).toHaveProperty('props.message', 'No tags to show');
+  });
+
+  it('should return a message when no tags are passed with specefied message', () => {
+    const data: string[] = [];
+    const message = 'Nessun tag da mostrare';
+
+    expect(<Tag data={data} message={message} />).toHaveProperty('props.message', message);
+  });
+
+  it('should return a list of tags when tags are passed', () => {
+    const data: string[] = ['tag1', 'tag2', 'tag3'];
+
+    expect(<Tag data={data} />).toHaveProperty('props.data', data);
+  });
+
+  it('should return a specefic number of tags when tags are passed with specefied number to show', () => {
+    const data: string[] = ['tag1', 'tag2', 'tag3'];
+    const tagsToShow = 2;
+
+    expect(<Tag data={data} tagsToShow={tagsToShow} />).toHaveProperty('props.tagsToShow', tagsToShow);
+  });
+});
