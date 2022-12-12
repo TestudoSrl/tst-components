7dbf67a641e8b54e3aeb6bcec578b4dc4e633286
Arash Nouri <arash.nouri@testudosrl.com>
2022-12-12 13:20:33 +0100

    add tag component

---
+++ README.md
@@ -0,0 +1,1 @@
+# Testudo tags component
---
+++ src/components/index.tsx
@@ -0,0 +1,44 @@
+import React, { type FC } from 'react';
+import { Box, Chip } from '@mui/material';
+
+interface TagProps {
+  data: string[];
+  showAll?: boolean;
+  tagsToShow?: number;
+  size?: 'small' | 'medium';
+  variant?: 'outlined' | 'filled';
+  color?: 'primary' | 'secondary' | 'success' | 'error' | 'warning' | 'info' | 'default';
+  message?: string;
+  margin?: number;
+}
+
+export const Tag: FC<TagProps> = (props) => {
+  const { data, showAll, tagsToShow, size, variant, color, message, margin } = props;
+
+  if (data.length === 0) return <div>{message}</div>;
+
+  if (data && !showAll) {
+    data.splice(tagsToShow ?? 1, data.length - (tagsToShow ?? 1));
+  }
+
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+      }}
+    >
+      {data.map((tag) => (
+        <Chip key={tag} label={tag} size={size} variant={variant} color={color} style={{ margin }} />
+      ))}
+    </Box>
+  );
+};
+
+Tag.defaultProps = {
+  showAll: false,
+  tagsToShow: 1,
+  size: 'small',
+  variant: 'outlined',
+  color: 'success',
+  message: 'No tags to show',
+};
