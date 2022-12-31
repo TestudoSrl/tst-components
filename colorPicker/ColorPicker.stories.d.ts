1f1e40f61e7df26f71c8589e5d1f39e9e0a07d25
Fulvio Cazzanti <fulvio.cazzant@testudosrl.com>
2022-12-31 15:24:21 +0100

    color picker

--- colorPicker/ColorPicker.stories.d.ts
+++
@@ -1,6 +0,0 @@
-import type { ComponentStory } from '@storybook/react';
-import React from 'react';
-import ColorPicker from './ColorPicker';
-declare const _default: ComponentMeta<React.FC<import("./ColorPicker").ColorPickerProps>>;
-export default _default;
-export declare const Default: ComponentStory<typeof ColorPicker>;
--- colorPicker/ColorPicker.stories.js
+++
@@ -1,9 +0,0 @@
-import { __assign } from "tslib";
-import React from 'react';
-import ColorPicker from './ColorPicker';
-export default {
-    title: 'Components/ColorPicker',
-    component: ColorPicker
-};
-export var Default = function (props) { return (React.createElement(ColorPicker, __assign({}, props))); };
-//# sourceMappingURL=ColorPicker.stories.js.map
